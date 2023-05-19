c2041df27326c65c75d78f9603bfa3dbe9fbdcd4
Arunkumar <arunkumar.s@indiumsoft.com>
2023-05-19 16:55:12 +0530

    initial commit

---
+++ apps/orders/src/orders.service.ts
@@ -0,0 +1,35 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateOrderRequest } from './dto/create.order.request';
+import { OrderRepository } from './order.repository';
+import { BILLING_SERVICE } from './constants/services';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable()
+export class OrdersService {
+  constructor(
+    private readonly orderRepository: OrderRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
+
+  async createOrder(request: CreateOrderRequest) {
+    const session = await this.orderRepository.startTransaction();
+    try {
+      const order = await this.orderRepository.create(request, { session });
+      await lastValueFrom(
+        this.billingClient.emit('order_created', {
+          request,
+        }),
+      );
+      await session.commitTransaction();
+      return order;
+    } catch (error) {
+      await session.abortTransaction();
+      throw error;
+    }
+  }
+
+  async getOrders() {
+    return await this.orderRepository.find({});
+  }
+}
